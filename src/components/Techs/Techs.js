dcfe824351a49be7d5891c37ce54ccca481e9c08
Denis Lozhkin <ssdenis00@yandex.ru>
2021-10-24 17:19:16 +0300

    feat: add techs in main

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,88 @@
+import "./Techs.css";
+
+function Techs() {
+  return (
+    <section className="techs section">
+      <h2 className="section__title">Технологии</h2>
+      <h3 className="techs__title">7 технологий</h3>
+      <p className="techs__subtitle">
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <ul className="techs__list">
+        <li className="techs__item">
+          <a
+            href="http://htmlbook.ru/html"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            HTML
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="http://htmlbook.ru/css/help"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            CSS
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="https://learn.javascript.ru/"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            JS
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="https://ru.reactjs.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            React
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="https://git-scm.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            Git
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="https://expressjs.com/ru/"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            Express.js
+          </a>
+        </li>
+        <li className="techs__item">
+          <a
+            href="https://www.mongodb.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="techs__link"
+          >
+            mongoDB
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Techs;
