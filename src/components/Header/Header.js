37c9ff7f9658e8e78c58ca63a66e483ad5681ae7
Denis Lozhkin <ssdenis00@yandex.ru>
2021-10-24 15:40:01 +0300

    fix: change navTab to Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import "./Header.css";
 import Logo from "../Logo/Logo";
-import NavTab from "../NavTab/NavTab";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
   return (
     <header className="header header_type_landing app__padding">
       <Logo />
-      <NavTab />
+      <Navigation />
     </header>
   );
 }
